2b6d3eb5681d121d33e5648bd724db62ca533e45
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2020-12-01 08:38:55 -0800

    cleaned up video item. added default search

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -4,7 +4,7 @@ import './VideoItem.css';
 const VideoItem = ({ video, onVideoSelect }) => {
    return (
       <div className="video-item item" onClick={() => onVideoSelect(video)}>
-         <img src={video.snippet.thumbnails.medium.url} alt="video thumbnail" className="ui image" />
+         <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} className="ui image" />
          <div className="content">
             <div className="header">
                {video.snippet.title}
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
    const renderedList = videos.map((video, index) => {
-      return <VideoItem video={video} key={index} onVideoSelect={onVideoSelect} />
+      return <VideoItem video={video} key={video.id.videoId} onVideoSelect={onVideoSelect} />
    });
 
    return (
